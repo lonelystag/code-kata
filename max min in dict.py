16f82f208327f87967a2fdb3fa3ea2754a736246
Ramesh S <42843515+lonelystag@users.noreply.github.com>
2019-02-21 14:05:36 +0530

    Update max min in dict.py

--- max min in dict.py
+++ max min in dict.py
@@ -1,8 +1,6 @@
 a={200:'google',189:'amazon',450:'tesla',45:'lineage',87:'oracle'}
 b=a.keys()
-for i in a:
-  c=max(b)
-  d=min(b)
-  print (c, a[c])
-  print (d, a[d])
-  
+c=max(b)
+d=min(b)
+print (c, a[c])
+print (d, a[d])
